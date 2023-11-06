582fbfad7199446f79418641d6e736fd57317f9f
Maximilian Sindram <master@geotrend.de>
2023-11-06 09:57:21 +0100

    initial commit

---
+++ src/Test.vue
@@ -0,0 +1,18 @@
+<template>
+<p>{{solar}}</p>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  props: {
+    solar: {
+      type: Number,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
